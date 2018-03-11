b8f911c68ec2be3cbf72d3444ee0bcee88f89d8e
jnguye89 <Jackie@Jackies-MacBook-Pro.local>
2018-03-10 18:34:34 -0600

    file path

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.post("/api/contacts/create",function(req,res){
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build//public/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/public/index.html"));
 });
 
 app.listen(PORT, function() {
